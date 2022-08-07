bdbe36d26f0d75b4a8b916bf6ccd4ebf2043d6fe
Margarida <margaridammcm@gmail.com>
2022-08-06 22:00:19 -0300

    Exibindo posts

--- src/components/ListaPost.jsx
+++ src/components/ListaPost.jsx
@@ -8,12 +8,12 @@ const [posts, setPosts] = useState([])
 
 useEffect(()=>{
   busca(url, setPosts)
-}, [])
+}, [url])
 
   return(
     <section className="posts container">
       {posts.map((post)=>(
-        <Link className={`cartao-post cartao-post--${post.categoria}`}>
+        <Link className={`cartao-post cartao-post--${post.categoria}`}  to={`/posts/${post.id}`}>
           <article key={post.id}>
             <h3 className="cartao-post__titulo">
               {post.title}
