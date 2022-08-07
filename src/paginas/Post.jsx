0c383bbbda7b828553be2c1f057929a9d2d1c82b
Margarida <margaridammcm@gmail.com>
2022-08-06 22:02:48 -0300

    Tratando caso post inexistente

--- src/paginas/Post.jsx
+++ src/paginas/Post.jsx
@@ -1,15 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { busca } from "../api/api";
 import '../assets/css/post.css'
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 
 const Post = () => {
+  let history = useHistory()
+
   const { id } = useParams()
  
   const[post, setPost] = useState({})
 
   useEffect(()=>{
     busca(`/posts/${id}`, setPost)
+    .catch(()=>{
+      history.push('/404')
+    })
   },[id])
 
   return (
