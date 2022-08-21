3aec21153b744d10d1a28630feae61a928549239
Margarida <margaridammcm@gmail.com>
2022-08-21 16:32:01 -0300

    Simplify header

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,33 +1,20 @@
 import React from 'react'
-import imagem from '../assets/img/margarida.jpg'
+import imageHome from '../assets/img/margarida.jpg'
+import imageNewPost from '../assets/img/write2.png'
 import '../assets/css/componentes/cabecalho.css'
 import { Link } from 'react-router-dom'
 
 const Cabecalho = () => {
   return (
-    <header className='cabecalho container'>
-      <div className='menu-hamburger'>
-        <span className='menu-hamburger__icone'>
-        </span>
-      </div>
-      <div className='cabecalho-container'>
-        <Link to='/' className='flex flex--centro'>
-          <img className='cabecalho__logo' src={imagem} alt ='Logo'/>
-          <h1 className='cabecalho__titulo'></h1>
-        </Link>
-      </div>
-      <nav className='menu-cabecalho'>
-        <ul className='menu-itens'>
-          <li><Link to="/novopost" className='menu-item menu-item--entrar'>Novo post</Link></li>
-          {/* <li><Link to="#" className='menu-item menu-item--entrar'>Entrar</Link></li>
-          <li><Link to="/" className='menu-item'>Blog</Link></li>
-          <li><Link to="/sobre" className='menu-item'>Sobre</Link></li> */}
-        </ul>
-      </nav>
-      <div className='menu-cabecalho-background'>
-
-      </div>
-    </header>
+    <div className='cabecalho'>
+      <Link to='/'>
+        <img className='cabecalho__logo' src={imageHome} alt ='Logo'/>
+        {/* <h1 className='cabecalho__titulo'></h1> */}
+      </Link>
+      <Link to="/novopost">
+      <img className='cabecalho__new_post' src={imageNewPost} alt ='Logo' />
+      </Link>
+    </div>
   )
 }
 
