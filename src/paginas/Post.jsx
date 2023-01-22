d523fdfdf452d1cdbfdd16cd1e009f46a88f7728
Margarida <margaridammcm@gmail.com>
2023-01-21 21:29:55 -0300

    separate with state and withou

--- src/paginas/Post.jsx
+++ src/paginas/Post.jsx
@@ -6,53 +6,14 @@ import Botao from '../components/Botao';
 import ReactMarkdown from 'react-markdown'
 import rehypeHighlight from 'rehype-highlight'
 
-const Post = () => {
-
-  const { id } = useParams()
-
-  const [titulo, setTitulo] = useState('')
-  const [descricao, setDescricao] = useState('')
-  const [markdown, setMarkdown] = useState('')
-  const [categoria, setCategoria] = useState('')
-  const [subcategoria, setSubcategoria] = useState('')
-  const [imagem, setImagem] = useState('')
-
-  useEffect(() => {
-    async function buscarDados() {
-      const res = await http.get(`/posts/${id}`)
-      const data = await res.data
-      console.log('data post', data)
-      setTitulo(data.title)
-      setDescricao(data.metadescription)
-      setMarkdown(data.markdown)
-      setCategoria(data.postCategory.categoryName)
-      setSubcategoria(data.postCategory.subCategoryName)
-      setImagem(data.image)
-    }
-    buscarDados();
-  }, [id])
-
-  if (Object.keys({titulo, categoria, subcategoria, markdown, descricao}).length === 0){
-    // TODO: fix empty state
-    return "carregando"
-  }
-
-  const httpRequestMethod = (method, url) => {
-    return method(url, {
-        title: titulo,
-        metadescription: descricao,
-        markdown: markdown,
-        postCategory: categoria,
-        postSubCategory: subcategoria,
-        image: imagem
-      })
-  }
-
-  const deletarPost = (ev)=> {
-    httpRequestMethod(http.delete, `posts/${id}`).then( () => {
-      window.location.pathname = `/`;
-    })
-  }
+const PostSemEstado = ({
+  subcategoria,
+  titulo,
+  descricao,
+  imagem,
+  id,
+  texto
+}) => {
   
   const renderActionButtons = () => (
     <div>
@@ -63,29 +24,87 @@ const Post = () => {
     </div>
   )
 
-  const renderMarkdown = () => {
+  const rendertexto = () => {
     return (
-      <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{markdown}</ReactMarkdown>
+      <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{texto}</ReactMarkdown>
     )
+  }
+
+  const httpRequestMethod = (method, url) => {
+    return method(url, {
+      title: titulo,
+      image: imagem,
+      description: descricao,
+      text: texto,
+      subcategoria_id: subcategoria,
+    })
+  }
+
+  const deletarPost = (ev) => {
+    httpRequestMethod(http.delete, `posts/${id}`).then(() => {
+      window.location.pathname = `/`;
+    })
   }
 
   return (
     <main className="container flex flex--centro">
-      <article className={`cartao-post cartao-post--${categoria}`}>
+      <article className={`cartao-post cartao-post--${subcategoria}`}>
         <h2 className="cartao__titulo">
           {titulo}
         </h2>
         <p className="cartao__descricao">
           {descricao}
         </p>
-        <img className="cartao__imagem" src={imagem} alt={descricao}/>
+        <img className="cartao__imagem" src={imagem} alt={descricao} />
         <div>
-          {renderMarkdown()}
+          {rendertexto()}
         </div>
         {renderActionButtons()}
       </article>
     </main>
   )
+}
+
+
+const Post = () => {
+  const { id } = useParams()
+  const [titulo, setTitulo] = useState('')
+  const [imagem, setImagem] = useState('')
+  const [descricao, setDescricao] = useState('')
+  const [texto, setTexto] = useState('')
+  const [subcategoria, setSubcategoria] = useState('')
+
+  useEffect(() => {
+    async function buscarDados() {
+      const res = await http.get(`/posts/${id}`)
+      const data = await res.data
+      console.log('data post', data)
+      setTitulo(data.title)
+      setImagem(data.image)
+      setDescricao(data.description)
+      setTexto(data.text)
+      setSubcategoria(data.subcategoria_id)
+    }
+    buscarDados();
+  }, [id])
+
+  if (Object.keys({ titulo, imagem, descricao, texto, subcategoria }).length === 0) {
+    // TODO: fix empty state
+    return "carregando"
+  }
+
+  return (
+    <PostSemEstado
+      titulo={titulo}
+      imagem={imagem}
+      descricao={descricao}
+      texto={texto}
+      subcategoria={subcategoria}
+    />
+  )
+
+
+
 }
 
 export default Post
