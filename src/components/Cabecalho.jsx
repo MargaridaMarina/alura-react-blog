a32baa27f1ff1743c6ec8d3b2c9395707745e0eb
Margarida <margaridammcm@gmail.com>
2023-01-26 20:49:17 -0300

    create new icons to header

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -1,18 +1,25 @@
 import React from 'react'
-import imageHome from '../assets/img/casa8.png'
-import imageNewPost from '../assets/img/write3.png'
+import imagePosts from '../assets/img/posts.png'
+import imageNewPost from '../assets/img/write.png'
+import imageNewProduct from '../assets/img/shopping.png'
+import imageNewRecipe from '../assets/img/recipe-book.png'
 import '../assets/css/componentes/cabecalho.css'
 import { Link } from 'react-router-dom'
 
 const Cabecalho = () => {
   return (
     <div className='cabecalho'>
-      <Link to='/'>
-        <img className='cabecalho__logo' src={imageHome} alt ='Logo'/>
-        {/* <h1 className='cabecalho__titulo'></h1> */}
+      <Link to='/posts'>
+        <img className='cabecalho__logo' src={imagePosts} alt ='Logo'/>
       </Link>
-      <Link to="/novopost">
-      <img className='cabecalho__new_post' src={imageNewPost} alt ='Logo' />
+      <Link to="/post/novo">
+        <img className='cabecalho__logo' src={imageNewPost} alt ='Logo' />
+      </Link>
+      <Link to="/produtos">
+        <img className='cabecalho__logo' src={imageNewProduct} alt ='Logo' />
+      </Link>
+      <Link to="/receitas">
+        <img className='cabecalho__logo' src={imageNewRecipe} alt ='Logo' />
       </Link>
     </div>
   )
