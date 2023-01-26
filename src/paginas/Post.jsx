92508637a6fd181defd003b8f184909ecb9bf005
Margarida <margaridammcm@gmail.com>
2023-01-25 22:21:28 -0300

    separate PostSemEstado

--- src/paginas/Post.jsx
+++ src/paginas/Post.jsx
@@ -1,69 +1,8 @@
 import React, { useEffect, useState } from "react";
 import http from "../api/api";
 import '../assets/css/post.css'
-import { useParams, Link } from "react-router-dom";
-import Botao from '../components/Botao';
-import ReactMarkdown from 'react-markdown'
-import rehypeHighlight from 'rehype-highlight'
-
-const PostSemEstado = ({
-  subcategoria,
-  titulo,
-  descricao,
-  imagem,
-  id,
-  texto
-}) => {
-  
-  const renderActionButtons = () => (
-    <div>
-      <Botao>
-        <Link to={`/editarpost/${id}`}>Editar</Link>
-      </Botao>
-      <Botao onClick={deletarPost}> Deletar </Botao>
-    </div>
-  )
-
-  const rendertexto = () => {
-    return (
-      <ReactMarkdown rehypePlugins={[rehypeHighlight]}>{texto}</ReactMarkdown>
-    )
-  }
-
-  const httpRequestMethod = (method, url) => {
-    return method(url, {
-      title: titulo,
-      image: imagem,
-      description: descricao,
-      text: texto,
-      subcategoria_title: subcategoria,
-    })
-  }
-
-  const deletarPost = (ev) => {
-    httpRequestMethod(http.delete, `posts/${id}`).then(() => {
-      window.location.pathname = `/`;
-    })
-  }
-
-  return (
-    <main className="container flex flex--centro">
-      <article className={`cartao-post cartao-post--${subcategoria}`}>
-        <h2 className="cartao__titulo">
-          {titulo}
-        </h2>
-        <p className="cartao__descricao">
-          {descricao}
-        </p>
-        <img className="cartao__imagem" src={imagem} alt={descricao} />
-        <div>
-          {rendertexto()}
-        </div>
-        {renderActionButtons()}
-      </article>
-    </main>
-  )
-}
+import { useParams } from "react-router-dom";
+import PostSemEstado from "./PostSemEstado";
 
 
 const Post = () => {
