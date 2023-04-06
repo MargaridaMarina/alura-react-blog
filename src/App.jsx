9f5a0c881eb669adcbf0971fa7043a8a3f8c6517
Margarida <margaridammcm@gmail.com>
2023-04-06 15:17:43 -0300

    rearranje routes

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,13 @@ import PostsPorSubcategoria from './paginas/PostsPorSubcategoria'
 import ListaCategorias from './components/ListaCategorias'
 import ListaPosts from './components/ListaPosts'
 import Posts from './paginas/Posts'
-import Sobre from './paginas/Sobre'
 import ListaSubcategorias from './components/ListaSubcategorias'
+import ListaLojas from './components/ListaLojas'
+import ListaReceitas from './components/ListaReceitas'
+import LojasPorSubcategoria from './paginas/LojasPorSubcategoria'
+import Lojas from './paginas/Lojas'
+import Sobre from './paginas/Sobre'
+
 
 function App() {
 
@@ -19,28 +24,50 @@ function App() {
       <Cabecalho/>
       <Switch>
         <Route exact path='/posts'>
-          <ListaCategorias/>         
+          <ListaCategorias origem='posts'/>     
+          <ListaSubcategorias origem='posts'/>
           <ListaPosts/>
         </Route>
-        {/* <Route path='/sobre'>
-          <Sobre/>
-        </Route> */}
-        <Route path='/categorias/:categoriaTitle'>
-          <ListaCategorias/>
-          <ListaSubcategorias/>
-          <ListaPosts/>
+        <Route exact path='/categorias/:categoriaTitle'>
+          <ListaCategorias origem='posts'/>     
+          <ListaSubcategorias origem='posts'/>
         </Route>
-        <Route path='/posts/subcategorias/:subcategoriaTitle'>
+        <Route exact path='/posts/subcategorias/:subcategoriaTitle'>
           <PostsPorSubcategoria/>
         </Route>
-        <Route path='/posts/:id'>
+        <Route exact path='/posts/:id'>
           <Posts/>
         </Route>
-        <Route path='/post/novo'>
+        <Route exact path='/editarPost/:id'>
+          <Formulario/>
+        </Route>
+        <Route exact path='/lojas'>
+          <ListaCategorias origem='lojas'/>         
+          <ListaSubcategorias origem='lojas'/>
+          <ListaLojas/>
+        </Route>
+        <Route exact path='/lojas/subcategorias/:subcategoriaTitle'>
+          <LojasPorSubcategoria/>
+        </Route>
+        <Route exact path='/lojas/:id'>
+          <Lojas/>
+        </Route>
+        <Route exact path='/loja/nova'>
           <Formulario isNew/>
         </Route>
-        <Route path='/editarpost/:id'>
+        <Route exact path='/editarLoja/:id'>
           <Formulario/>
+        </Route>
+        <Route exact path='/receitas'>
+          <ListaCategorias origem='receitas'/>         
+          <ListaSubcategorias origem='receitas'/>
+          <ListaReceitas/>
+        </Route>
+        <Route exact path='/sobre'>
+          <Sobre/>
+        </Route>
+        <Route exact path='/post/novo'>
+          <Formulario isNew/>
         </Route>
         <Route>
           <Pagina404/>
